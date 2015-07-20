e51392b7622a3aa8c3b1cbf69216792cf645fd70
Shannon Forsman <sforsman1@gmail.com>
2015-07-20 12:49:20 -0600

    small change

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(passport.initialize());
+app.use(passport.initialize())
 
 
 passport.use(new LinkedInStrategy({
